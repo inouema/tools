3e20c607d0234265b07437cddbaf9fc97bfd806f
MasatoINOUE <inouema@inouema-VirtualBox.(none)>
2015-11-07 00:32:51 +0900

    add

---
+++ sh/snmptrap.sh
@@ -0,0 +1,2 @@
+sudo snmptrap -v 1 -c public 192.168.11.32:162 .1.3.6.1.4.1.207.8.48.1 '' 6 1 '' .1.3.6.1.4.1.8072.99.1 s "test"
+                             # to               enterprise.oid         host ? ? uptime oid  type msg
